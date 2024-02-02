8b969e66b403810baa64fe348bf84dfa9b13febe
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2024-02-02 16:07:47 -0300

    Added more tests to TaskService

--- src/test/java/com/example/springCrud/TaskServiceTest.java
+++ src/test/java/com/example/springCrud/TaskServiceTest.java
@@ -2,11 +2,13 @@ package com.example.springCrud;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.example.springCrud.dtos.TaskRecordDto;
 import com.example.springCrud.model.Task;
 import com.example.springCrud.model.User;
 import com.example.springCrud.model.UserPrincipal;
@@ -32,39 +35,58 @@ public class TaskServiceTest {
     @InjectMocks
     private TaskService taskService;
 
-    private User testUser;
-    private Task testTask;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-
-        testUser = new User();
-        testUser.setUser_id(1);
-
-        testTask = new Task();
-        testTask.setId(1L);
-        testTask.setUser(testUser);
-    }
     @Test
     public void testCreateTask() {
-        when(taskRepository.save(any(Task.class))).thenReturn(testTask);
+        UserPrincipal userPrincipal = new UserPrincipal(new User());
+        Task task = new Task();
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
 
-        Task createdTask = taskService.createTask(new Task(), new UserPrincipal(testUser));
+        Task createdTask = taskService.createTask(new Task(), userPrincipal);
         assertNotNull(createdTask);
-        assertEquals(testUser.getUser_id(), createdTask.getUser().getUser_id());
+        assertEquals(task, createdTask);
     }
 
     @Test
     public void testGetTasksForUser() {
+        UserPrincipal userPrincipal = new UserPrincipal(new User());
         List<Task> tasks = new ArrayList<>();
-        tasks.add(testTask);
 
-        when(taskRepository.findByUser(testUser)).thenReturn(tasks);
+        when(taskRepository.findByUser(any(User.class))).thenReturn(tasks);
 
-        ResponseEntity<List<Task>> responseEntity = taskService.getTasksForUser(new UserPrincipal(testUser));
+        ResponseEntity<List<Task>> responseEntity = taskService.getTasksForUser(userPrincipal);
         assertNotNull(responseEntity);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals(tasks, responseEntity.getBody());
     }
+
+    @Test
+    public void testGetTestById() {
+        Task task = new Task();
+        task.setId(1L);
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        ResponseEntity<Task> responseEntity = taskService.getTaskById(1L);
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(task, responseEntity.getBody());
+    }
+
+    @Test
+    public void testDeleteTask() {
+        UserPrincipal userPrincipal = new UserPrincipal(new User());
+        
+        Task task = new Task();
+        task.setId(1L);
+        task.setUser(userPrincipal.getUser());
+
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+
+        ResponseEntity<Object> responseEntity = taskService.deleteTask(1L, userPrincipal);
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertTrue(responseEntity.getBody().toString().contains("Task deleted sucessfully"));
+    }
+
+
 }
